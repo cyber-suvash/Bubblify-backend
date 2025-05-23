ec73bf41c0673b7aa7bc9bc62a19a1eee5756910
cyber-suvash <suvashd2018@gmail.com>
2025-05-23 23:47:17 +0530

    url chages

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 // 🧩 Middlewares
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://bublify-e-commerce.vercel.app'],
+  origin: ['http://localhost:5173', 'https://bubblify-e-commerce.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
